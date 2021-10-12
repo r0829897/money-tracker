07c1dad4ff981a2acdb5da19a0cffd6fcf9b2920
Yoran Mertens <yoran.mertens@student.kuleuven.be>
2021-10-12 12:25:31 +0200

    scrolling mechanism

--- components/Payments.js
+++ components/Payments.js
@@ -1,48 +1,25 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, FlatList, SafeAreaView } from "react-native";
 import PoppinsText from "./PoppinsText";
 import PaymentItem from "./PaymentItem";
 import { URL_SERVER } from "../config";
 
 export default Payments = ({ payments }) => {
-  // const DUMMY_PAYMENTS = [
-  //   {
-  //     id: 1,
-  //     title: "Food",
-  //     amount: -20,
-  //     date: "25 okt",
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "Student Job",
-  //     amount: 50,
-  //     date: "21 okt",
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "Food",
-  //     amount: -15,
-  //     date: "15 okt",
-  //   },
-  //   {
-  //     id: 4,
-  //     title: "Shopping",
-  //     amount: -30,
-  //     date: "9 okt",
-  //   },
-  //   {
-  //     id: 5,
-  //     title: "Equipment",
-  //     amount: -7,
-  //     date: "5 okt",
-  //   },
-  // ];
+  const renderItem = ({ item }) => (
+    <PaymentItem
+      // key={item._id}
+      id={item._id}
+      title={item.title}
+      amount={item.amount}
+      date={item.date.split("T")[0]}
+    />
+  );
 
   return (
-    <View>
+    <SafeAreaView style={{ flex: 1 }}>
       <PoppinsText style={styles.header}>Recent Payments</PoppinsText>
       <View style={styles.payments}>
-        {payments.map((payment) => (
+        {/* {payments.map((payment) => (
           <PaymentItem
             key={payment._id}
             id={payment._id}
@@ -50,9 +27,14 @@ export default Payments = ({ payments }) => {
             amount={payment.amount}
             date={payment.date.split("T")[0]}
           />
-        ))}
+        ))} */}
+        <FlatList
+          data={payments}
+          renderItem={renderItem}
+          keyExtractor={(item) => item._id.toString()}
+        />
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
@@ -64,8 +46,11 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginBottom: 20,
   },
+  // payments: {
+  //   display: "flex",
+  //   flexDirection: "column",
+  // },
   payments: {
-    display: "flex",
-    flexDirection: "column",
+    paddingBottom: 50,
   },
 });
